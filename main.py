fb2e32142e1dd8c9278eb125c50a460c1fedd36e
itsMe-ThatOneGuy <matthewsmart13@gmail.com>
2024-06-05 22:33:31 -0500

    helper funcs to get and split str

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 def main():
-    
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    book_path = "books/frankenstein.txt"
+    text = get_text(book_path).lower()
+    num_words = get_num_words(text)
+    print(num_words)
 
-    print(file_contents)
+def get_text(book):
+    with open(book) as f:
+        return f.read()
+    
+def get_num_words(text):
+    words = text.split()
+    return len(words) 
+
 main()
